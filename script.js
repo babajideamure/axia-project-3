6be7554c7e533ee453d99cbc833a2da5688982d7
Babajide Amure <145385180+babajideamure@users.noreply.github.com>
2025-03-27 13:59:49 +0100

    in progress

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("data.json") 
+    .then(response => response.json())
+    .then(data => {
+      const jobsContainer = document.querySelector(".jobs");
+      jobsContainer.innerHTML = ""; 
+
+      data.forEach(job => {
+        const jobElement = document.createElement("div");
+        jobElement.classList.add("job-items");
+
+        jobElement.innerHTML = `
+          <div class="jobs-column job-column-left">
+            <img src="${job.logo}" alt="${job.company} logo" class="job-logo">
+            <div class="job-info">
+              <span class="jobs-company">${job.company}</span>
+              <span class="jobs-title">${job.position}</span>
+              
+              <ul class="jobs-details">
+                <li>${job.postedAt}</li>
+                <li>${job.contract}</li>
+                <li>${job.location}</li>
+              </ul>
+            </div>
+          </div>
+          <div class="jobs-column job-column-right">
+            ${[...job.languages, ...job.tools].map(tag => `<span class="tags">${tag}</span>`).join('')}
+          </div>
+        `;
+
+        jobsContainer.appendChild(jobElement);
+      });
+    })
+    .catch(error => console.error("Error loading job listings:", error));
+});
